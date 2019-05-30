c6566abc49e2ea19f3ab8de44e017e2db7e50456
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-05-30 21:13:26 +0200

    fizzbuzzconverter

--- src/test/java/FizzBuzzTDDTest.java
+++ src/test/java/FizzBuzzTDDTest.java
@@ -1,6 +1,32 @@
+import org.junit.Test;
+
 public class FizzBuzzTDDTest {
 
     // Fizz buzz (often spelled FizzBuzz in this context) has been used as an interview screening device for computer programmers.
     // Writing a program to output the first 100 FizzBuzz numbers is a trivial problem for any would-be computer programmer,
     // so interviewers can easily sort out those with insufficient programming ability.
+
+
+    /*
+    1==1
+    2==2
+    3==Fizz
+    4==4
+    5==Buzz
+    6==Fizz
+    7==7
+    8=8
+    9==Fizz
+    10==Buzz
+    15==FizzBuzz
+    100 == Buzz
+     */
+
+    @Test
+    public void fizzBuzzConverterLeavesNormalNumbersAlone(){
+       fizzBuzz = new FizzBuzzConverter();
+
+    }
+
+
 }
